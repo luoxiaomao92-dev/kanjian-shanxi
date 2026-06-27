980ec7f22d97e75c3f4b1392da971834db94e7aa
luoxiaomao92-dev <luoxiaomao92@gmail.com>
2026-06-27 21:47:15 +0800

    Fix production image paths

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        "place-detail": "place-detail.html",
+        prototype: "prototype.html"
+      }
+    }
+  }
 });
